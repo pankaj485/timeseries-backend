ff64bbb7c6d078ce444063fee143e4beeaa7a838
Pankaj Thapaliya <pankaj.thapaliya@enginecal.com>
2024-01-11 10:26:56 +0530

    basic project setup

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("<h1> Express + TypeScript Server</h1>");
+  res.end();
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
